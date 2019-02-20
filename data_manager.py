3731c846974798f9c7983426dd01e0f39f40f21b
balazske96 <venyigebalazs96@gmail.com>
2019-02-20 13:41:09 +0100

    delete_ functions merged to one 'delete_line_from_csv(), added a get_list_by_key()

--- data_manager.py
+++ data_manager.py
@@ -3,7 +3,7 @@ import time
 import util
 
 
-def new_question(form_title, form_question):
+def add_question(form_title, form_question):
     momentary_time = round(time.time())
     new_id = util.get_id('data/question.csv')
     nested_ordered_dict = connection.import_csv('data/question.csv')
@@ -19,7 +19,7 @@ def new_question(form_title, form_question):
     connection.export_csv('data/question.csv', nested_ordered_dict)
 
 
-def new_answer(question_id, form_answer):
+def add_answer(question_id, form_answer):
     momentary_time = round(time.time())
     new_id = util.get_id('data/answer.csv')
     nested_ordered_dict = connection.import_csv('data/answer.csv')
@@ -46,23 +46,27 @@ def get_ordered_dict_by_id(filename, id):
             return row
 
 
-def delete_answer_from_csv(id_):
-    table = connection.import_csv('data/answer.csv')
+def get_list_by_key(filename, id, key):
+    """
+    :param filename: relative path
+    :param id: input id as integer
+    :param key: the key of the dictionary where i searh for the id
+    :return: one ordered dict
+    """
+    result = []
+    nested_ordered_dicts = connection.import_csv(filename)
+    for row in nested_ordered_dicts:
+        if int(row[key]) == id:
+            result.append(row)
+    return result
+
+
+def delete_line_from_csv(filename, id_):
+    table = connection.import_csv(filename)
     temp = []
     for answer in table:
         if answer['id'] == id_:
             pass
         else:
             temp.append(answer)
-    connection.export_csv('data/answer.csv', temp)
-
-
-def delete_question_from_csv(id_):
-    table = connection.import_csv('data/question.csv')
-    temp = []
-    for answer in table:
-        if answer['id'] == id_:
-            pass
-        else:
-            temp.append(answer)
-    connection.export_csv('data/question.csv', temp)
+    connection.export_csv(filename, temp)
