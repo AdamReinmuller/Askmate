37541aa44d7643911585ed91d718802bdce2c085
Rónai Zsuzsanna <ronai.zsuzsanna@gmail.com>
2019-02-20 14:05:15 +0100

    server.py: question/question_id, index, q/q_id/new_answer, index.html, question.html

--- server.py
+++ server.py
@@ -7,6 +7,36 @@ import data_manager
 app = Flask(__name__)
 
 
+@app.route('/')
+@app.route('/list')
+def route_index():
+    questions = connection.import_csv('data/question.csv')
+    header = util.get_headers('data/question.csv')[4]
+
+    return render_template('index.html', questions=questions, header=header)
+
+
+@app.route('/question/<int:question_id>')
+def route_question(question_id):
+    question = data_manager.get_ordered_dict_by_id('data/question.csv', question_id)
+    headers_q = util.get_headers('data/question.csv')
+    answers = data_manager.get_list_by_key('data/answer.csv', question_id, 'question_id')
+    headers_a = util.get_headers('data/answer.csv')
+    return render_template('question.html', question=question, headers_q=headers_q, answers=answers,
+                           headers_a=headers_a)
+
+@app.route('/question/<int:question_id>/new-answer', methods=['GET', 'POST'])
+def post_answer(question_id):
+    if request.method == 'GET':
+        single_question = data_manager.get_ordered_dict_by_id('data/question.csv', int(question_id))
+        question_message = single_question['message']
+        return render_template('post-answer.html', question_message=question_message)
+    elif request.method == 'POST':
+        form_answer = request.form['answer_message']
+        data_manager.new_answer(question_id, form_answer)
+        return redirect('/question/<int:question_id>')
+
+
 @app.route('/add-question', methods=['GET', 'POST'])
 def add_new_question():
     if request.method == 'GET':
@@ -24,7 +54,7 @@ def delete_answer(answer_id=None):
 
 
 @app.route('/answer/<question_id>/delete')
-def delete_answer(question_id=None):
+def delete_question(question_id=None):
     data_manager.delete_question_from_csv(question_id)
     return redirect('/add-question')
 
