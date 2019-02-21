664767aa1cef64e787b18272ab71e2330939cc15
Rónai Zsuzsanna <ronai.zsuzsanna@gmail.com>
2019-02-21 15:23:27 +0100

    server.py with list sort and list.html with sort option

--- server.py
+++ server.py
@@ -9,7 +9,24 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    questions = sorted(connection.import_csv('data/question.csv'), key=lambda k: k['id'], reverse=True)
+    questions = sorted(connection.import_csv('data/question.csv'), key=lambda k: int(k['id']), reverse=True)
+    headers = util.get_headers('data/question.csv')
+
+    return render_template('list.html', questions=questions, headers=headers)
+
+
+@app.route('/list/')
+def route_sorted_list():
+    try:
+        if request.args['order_direction'] == 'desc':
+            questions = sorted(connection.import_csv('data/question.csv'), key=lambda k: int(k[request.args['order_by']]), reverse=True)
+        else:
+            questions = sorted(connection.import_csv('data/question.csv'), key=lambda k: int(k[request.args['order_by']]), reverse=False)
+    except:
+        if request.args['order_direction'] == 'desc':
+            questions = sorted(connection.import_csv('data/question.csv'), key=lambda k: k[request.args['order_by']], reverse=True)
+        else:
+            questions = sorted(connection.import_csv('data/question.csv'), key=lambda k: k[request.args['order_by']], reverse=False)
     headers = util.get_headers('data/question.csv')
 
     return render_template('list.html', questions=questions, headers=headers)
