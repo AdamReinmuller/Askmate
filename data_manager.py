ca9672ed1254be8c59768f01c2d7ec5731e3d27b
adamRajmuller <rajfta@gmail.com>
2019-03-06 11:19:02 +0100

    data_manager refactor add_question & answer

--- data_manager.py
+++ data_manager.py
@@ -1,82 +1,121 @@
 import connection
 import time
 import util
+import urllib.request
+import os
 
 
-def add_question(form_title, form_question):
-    momentary_time = round(time.time())
-    new_id = util.get_id('data/question.csv')
-    nested_ordered_dict = connection.import_csv('data/question.csv')
-    new_content = {'id': new_id,
-                   'submission_time': momentary_time,
-                   'view_number': 0,
-                   'vote_number': 0,
-                   'title': form_title,
-                   'message': form_question,
-                   'image': 'No image'
-                   }
-    nested_ordered_dict.append(new_content)
-    connection.export_csv('data/question.csv', nested_ordered_dict)
+question_db = 'question'
+answer_db = 'answer'
+comment_db = 'comment'
+tag_db = 'tag'
+question_tag_db = 'question_tag'
 
 
-def add_answer(question_id, form_answer):
-    momentary_time = round(time.time())
-    new_id = util.get_id('data/answer.csv')
-    nested_ordered_dict = connection.import_csv('data/answer.csv')
-    new_content = {'id': new_id,
-                   'submission_time': momentary_time,
-                   'vote_number': 0,
-                   'question_id': question_id,
-                   'message': form_answer,
-                   'image': 'No image'
-                   }
-    nested_ordered_dict.append(new_content)
-    connection.export_csv('data/answer.csv', nested_ordered_dict)
-
-
-def get_ordered_dict_by_id(filename, id):
+def get_table(table, key="", sort=""):
     """
-    :param filename: relative path
+    :key: by what fieldname you sort it
+    :sort: asc or desc
+    :return: questions in a list of ordered dictionaries
+    """
+    questions = connection.import_from_db(table)
+    if sort == 'desc':
+        try:
+            questions = sorted(questions, key=lambda x: int(x[key]), reverse=True)
+        except:
+            questions = sorted(questions, key=lambda x: x[key], reverse=True)
+    elif sort == 'asc':
+        try:
+            questions = sorted(questions, key=lambda x: int(x[key]), reverse=False)
+        except:
+            questions = sorted(questions, key=lambda x: x[key], reverse=False)
+    return questions
+
+
+def save_image_to_file(url, filename):
+    urllib.request.urlretrieve(url, filename)
+
+
+def delete_file(filename):
+    os.remove(filename)
+
+
+def check_file(filename):
+    return os.path.exists(filename)
+
+
+@connection.connection_handler
+def add_question(cursor, title, message):
+    """
+    :return: writes to question_db
+    """
+    current_time = util.get_time()
+    cursor.execute("""INSERT INTO question
+                      (submission_time, view_number, vote_number, title, message, image)
+                      VALUES ( %(submission_time)s, 0, 0, %(title)s, %(message)s, 'no image' )
+                    """,
+                   dict(submission_time=current_time, title=title, message=message)
+                   )
+
+
+@connection.connection_handler
+def add_answer(cursor, question_id, message):
+    """
+    :return: writes to answer_db
+    """
+    current_time = util.get_time()
+    cursor.execute("""INSERT INTO answer
+                      (submission_time, vote_number, question_id, message, image)
+                      VALUES ( %(submission_time)s, 0, %(question_id)s, %(message)s, 'no image' )
+                    """,
+                   dict(submission_time=current_time, question_id=question_id, message=message)
+                   )
+
+
+def get_ordered_dict_by_id(table, id):
+    """
+    :param table: database's table
     :param id: input id as integer
     :return: one ordered dict
     """
-    nested_ordered_dicts = connection.import_csv(filename)
+    nested_ordered_dicts = connection.import_from_db(table)
     for row in nested_ordered_dicts:
         if int(row['id']) == id:
             return row
 
 
-def get_list_by_key(filename, data, key):
+def get_list_by_key(table, data, key):
     """
-    :param filename: relative path
+    :param table: database's table
     :param data: input data
     :param key: the key of the dictionary where i searh for the data
     :return: one ordered dict
     """
     result = []
-    nested_ordered_dicts = connection.import_csv(filename)
+    nested_ordered_dicts = connection.import_from_db(table)
     for row in nested_ordered_dicts:
         if int(row[key]) == data:
             result.append(row)
     return result
 
-def get_list_by_not_key(filename, data, key):
+
+def get_list_by_not_key(table, data, key):
     """
-    :param filename: relative path
+    :param table: database's table
     :param data: input data
     :param key: the key of the dictionary where i searh for the data
     :return: one ordered dict
     """
     result = []
-    nested_ordered_dicts = connection.import_csv(filename)
+    nested_ordered_dicts = connection.import_from_db(table)
     for row in nested_ordered_dicts:
         if int(row[key]) != data:
             result.append(row)
     return result
 
 
-def get_key_by_id(filename, key, id):
-    nested_ordered_dicts = connection.import_csv(filename)
+def get_key_by_id(table, key, id):
+    nested_ordered_dicts = connection.import_from_db(table)
     for row in nested_ordered_dicts:
         if int(row['id']) == int(id):
             return row[key]
@@ -100,6 +139,14 @@ def edit_line_from_csv(filename, id_, title, message):
             row['title'] = title
             row['message'] = message
             break
+    connection.export_csv(filename, nested_ordered_dicts)
+
+
+def update_line_from_csv(filename, id_, key, new_data):
+    nested_ordered_dicts = connection.import_csv(filename)
+    for row in nested_ordered_dicts:
+        if int(row['id']) == id_:
+            row[key] = new_data
     connection.export_csv(filename, nested_ordered_dicts)
 
 
