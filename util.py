55d39bd80934aa6fbabd347d05ea98c02a757cda
balazske96 <venyigebalazs96@gmail.com>
2019-03-06 12:03:13 +0100

    get_headers delted because unnecessary

--- util.py
+++ util.py
@@ -1,12 +1,6 @@
 from datetime import datetime
 
 
-def get_headers(file):
-    file_all = connection.import_csv(file)
-    headers = list(file_all[0].keys())
-    return headers
-
-
 def get_time():
     dt = datetime.now()
     return dt
